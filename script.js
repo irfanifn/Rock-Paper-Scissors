7d0657cd31686b95c69b2cdcd5462f17c1ddfde5
irfan <ifan91881@gmail.com>
2025-04-24 18:21:47 +0800

    Create playRound function

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper") ||
+    (humanChoice === "Paper" && computerChoice === "Rock")
+  ) {
+    humanScore++;
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+  } else {
+    computerChoice++;
+    console.log(`You lose ${computerChoice} beats ${humanChoice}`);
+  }
+}
